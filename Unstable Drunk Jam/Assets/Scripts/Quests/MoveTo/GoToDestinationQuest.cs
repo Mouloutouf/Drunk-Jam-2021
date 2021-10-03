46a2f55412c98e69ead4dc7a65feae93e96f1fcb
Maxence <maxdurlon@gmail.com>
2021-10-03 17:14:49 +0200

    Game Works Alright

--- Unstable Drunk Jam/Assets/Scripts/Quests/MoveTo/GoToDestinationQuest.cs
+++ Unstable Drunk Jam/Assets/Scripts/Quests/MoveTo/GoToDestinationQuest.cs
@@ -1,17 +1,38 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
 
 public class GoToDestinationQuest : Quest
 {
-    public Transform destination;
+    public List<Transform> destinations;
+    private Transform destination;
 
     public Transform player;
 
     public float radius;
 
+    public override void StartQuest()
+    {
+        base.StartQuest();
+
+        int i = Random.Range(0, destinations.Count);
+        destination = destinations[i];
+
+        destination.gameObject.SetActive(true);
+    }
+
     public override bool Completion()
     {
         return Vector3.Distance(new Vector3(destination.position.x, 0f, destination.position.z), new Vector3(player.position.x, 0f, player.position.z)) <= radius;
     }
+
+    public override void EndQuest()
+    {
+        destination.gameObject.SetActive(false);
+
+        base.EndQuest();
+    }
+
     private void OnDrawGizmos()
     {
         if (destination == null) return;
